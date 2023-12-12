9cc0cba258cc3480b2f228d9aec2b0a113aa902e
alu0101515458 <alu0101515458@ull.edu.es>
2023-12-12 18:55:25 +0000

    corrigiendo espaciado

--- p11/main.cc
+++ p11/main.cc
@@ -3,34 +3,34 @@
 #include "point_set.h"
 
 /*
-    * Para compilar:
-    * make
-    * 
-    * Para ejecutar:
-    * ./program
-    * (DATOS DE ENTRADA)
-    * 5
-    * 1.0 2.0
-    * 3.4 5.6
-    * 7.8 9.0
-    * 2.3 4.5
-    * 6.7 8.9
-    * 
-    * Para limpiar:
-    * make clean
-    
+  * Para compilar:
+  * make
+  * 
+  * Para ejecutar:
+  * ./program
+  * (DATOS DE ENTRADA)
+  * 5
+  * 1.0 2.0
+  * 3.4 5.6
+  * 7.8 9.0
+  * 2.3 4.5
+  * 6.7 8.9
+  * 
+  * Para limpiar:
+  * make clean
 */
 int main() {
-    CyA::point_vector points;
+  // Crear un vector de puntos
+  CyA::point_vector points;
 
-    // Leer puntos desde la entrada estándar
-    std::cin >> points;
+  // Leer puntos desde la entrada estándar
+  std::cin >> points;
 
-    // Crear un point_set con los puntos leídos
-    point_set ps(points);
+  // Crear un point_set con los puntos leídos
+  point_set ps(points);
 
-    // Calcular el árbol de expansión mínima y emitir el resultado
-    ps.write_tree(std::cout);
+  // Calcular el árbol de expansión mínima y emitir el resultado
+  ps.write_tree(std::cout);
 
-    return 0;
+  return 0;
 }
